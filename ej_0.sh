18d61e5bc685120a14720a918acdae8e81e5f969
asus_fausto <faustogiannottiur@gmail.com>
2025-04-02 00:43:14 -0300

    versión final ej0 tp0 intro.

--- ej_0.sh
+++ ej_0.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
 sumar_energias() {
+    
+    #chequea que no estén vacíos
+    if [ -z "$1" ] || [ -z "$2" ]; then   
+        echo "hay que ingresar 2 energías"
+        exit 1 
+    fi
+
+    # usamos regexs para asegurarnos que sean numeros
+    #chequea que ambos sean números
+
+    if ! [[ "$1" =~ ^[0-9]+$  ]] || ! [[ "$2" =~ ^[0-9]+$  ]]; then
+        echo "las energías no son números válidos"
+        exit 1
+    fi
+    
     suma=$(( $1 + $2 ))
     echo $suma
 }
