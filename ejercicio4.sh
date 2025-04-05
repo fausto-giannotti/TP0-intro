24504892110a6d2b4720d759c0881fa9c3fbf19d
asus_fausto <faustogiannottiur@gmail.com>
2025-04-05 18:17:08 -0300

    agrego ejercico 4 y txt para testearlo

---
+++ ejercicio4.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+function contador_cold {
+    
+    # chequeamos si el archivo es existe y válido
+    if ! [ -f $1 ] || [ -z "$1"] ; then
+        echo "ingresar un archivo válido"
+        exit 1
+    fi
+    
+    # -i ignora case, -w coincide solo con palabras completas 
+    # -o separa cada coincidencia en una nueva linea (cada línea resultante es "cold")
+    # la pipeline | reedirige las lineas a wc -l (wc = word count, l = lines)
+    # que cuenta el número de estas
+
+    apariciones_cold=$(grep -i -w -o "cold" "$1" | wc -l)
+    
+    echo "$apariciones_cold"
+}
+
+contador_cold $1
