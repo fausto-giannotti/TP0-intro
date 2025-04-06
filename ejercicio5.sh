c35b3fe22c092c4637082bc708cc46c8cddc5ef2
asus_fausto <faustogiannottiur@gmail.com>
2025-04-06 12:07:20 -0300

    agrego ejercicio 5

---
+++ ejercicio5.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+function contador_lineas_con_num {
+    
+    # chequeamos si el archivo es existe y es válido
+    if ! [ -f $1 ] || [ -z "$1" ]; then
+        echo "ingresar un archivo válido"
+        exit 1
+    fi
+
+    lineas_con_num=$(grep -c '[0-9]'  "$1")
+    echo "$lineas_con_num"
+
+}
+
+contador_lineas_con_num $1
